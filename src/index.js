93a783c1800a2699035d43bded912f5b319f9437
Oreus-F <fourniol.lucas@gmail.com>
2025-05-22 21:56:10 +0200

    Delete txt test from index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import "./style.css";
 import { resetDOM } from "./resetpage";
 import { homepageLoading } from "./homepage";
 import { loadingMenu } from "./menu";
-import txt from "../../../Downloads/txt.txt";
 
 // homepageLoading();
 // loadingMenu();
-
-let array = txt.split(",")
-console.log(array)
-
-// Créer fichier text avec le text correct puis intégrer inside la bonne fonction
