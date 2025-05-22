eb639a65b8815c2a75b3a356e5de19639ab0c627
Oreus-F <fourniol.lucas@gmail.com>
2025-05-23 00:18:46 +0200

    Create function for LI / update the array and create Galette elements

--- src/menu.js
+++ src/menu.js
@@ -5,7 +5,36 @@ import liste_vins from "../asset/txt/liste_vins.txt";
 import liste_sans_alcool from "../asset/txt/liste_sans_alcool.txt";
 
 
+const create_galette = function(text){
+    const div = document.createElement("div");
+    div.setAttribute("class", "content-center title-img listing galettes");
 
+    const title = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const h3 = document.createElement("h3");
+    h2.textContent = "galettes";
+    h3.textContent = "farine de sarrasin (sans gluten*)";
+    title.appendChild(h2);
+    title.appendChild(h3);
+
+
+    const ul = document.createElement("ul");
+
+    let listeContent = text.split(";");
+    let liLength = (listeContent.length)/3;
+
+    for (let x = 0; x < liLength; x++ ){
+        const li = createLI(listeContent);
+        deleteLIElement(listeContent);
+        ul.appendChild(li);
+    };
+
+    div.appendChild(title);
+    div.appendChild(ul);
+
+    return div;
+
+}
 
 const createLI = function(text){
 
@@ -18,7 +47,11 @@ const createLI = function(text){
     p3.textContent = text[2];
     liste.appendChild(p1);
     liste.appendChild(p2);
+
+    if (p3 === "empty") return liste;
+
     liste.appendChild(p3);
+    
     return liste;
 
 }
@@ -33,16 +66,15 @@ const deleteLIElement = function(text){
 
 const loadingMenu = function(){
     const header = document.querySelector("header");
-   
-
-    const pageMenu = document.createElement("div")
+    if (header.getAttribute("class") !== "menu") header.classList.toggle("menu")
     
 
 
     const content = document.querySelector("#content");
-    
-    
 
+    const galette = create_galette(liste_galette);
+    
+    content.appendChild(galette);
 
 }
 
