bf0162d84a2866070d73842cd9a473e85f4a872f
Oreus-F <fourniol.lucas@gmail.com>
2025-05-23 18:28:56 +0200

    activate showModal for hardcoding it

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import { loading_homepage } from "./homepage";
 import { loadingMenu } from "./menu";
 
 // resetDOM();
-// loading_homepage();
+loading_homepage();
 // loadingMenu();
+
+const reservation = document.querySelector("#reservation");
+
+reservation.showModal();
+
