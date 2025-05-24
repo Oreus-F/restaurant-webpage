f72d5a8ab14ed380dbfad879ae7e6fddfcf11d23
Oreus-F <fourniol.lucas@gmail.com>
2025-05-25 00:45:22 +0200

    import calendar feature under CreateCalendar's name

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import "./style.css";
 import { resetDOM } from "./resetpage";
 import { loading_homepage } from "./homepage";
 import { loadingMenu } from "./menu";
-import * as calendar from "./calendar-test"
+import createCalendar from "./calendar-test";
+
 
 // resetDOM();
 // loading_homepage();
 // loadingMenu();
-
+createCalendar();
 // const reservation = document.querySelector("#reservation");
 
 // reservation.showModal();
