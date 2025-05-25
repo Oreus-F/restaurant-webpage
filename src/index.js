50529a08d1887f6a550a83caaf1ea11056db6b22
Oreus-F <fourniol.lucas@gmail.com>
2025-05-26 00:59:57 +0200

    keep menu open and call displayReservation

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import "./style.css";
 import { resetDOM } from "./resetpage";
 import { loading_homepage } from "./homepage";
 import { loadingMenu } from "./menu";
-import createCalendar from "./calendar";
+import displayReservation from "./calendar";
 
 
 // resetDOM();
 // loading_homepage();
-// loadingMenu();
-createCalendar();
-// const reservation = document.querySelector("#reservation");
-
-// reservation.showModal();
-
+loadingMenu();
+displayReservation();
