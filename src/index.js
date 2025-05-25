5c80d26dd32c77baecea84d75dab2e842ba8b8be
Oreus-F <fourniol.lucas@gmail.com>
2025-05-25 18:37:54 +0200

    change import name of calendar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 import { resetDOM } from "./resetpage";
 import { loading_homepage } from "./homepage";
 import { loadingMenu } from "./menu";
-import createCalendar from "./calendar-test";
+import createCalendar from "./calendar";
 
 
 // resetDOM();
