de55eb823199e5eac177cd8b7fa6524fffede534
Oreus-F <fourniol.lucas@gmail.com>
2025-06-02 23:50:11 +0200

    create text and link for AboutUs section

--- src/about_us.js
+++ src/about_us.js
@@ -1,4 +1,5 @@
-import Lorem from "../asset/txt/Lorem.txt"
+import TXT_AboutUS from "../asset/txt/AboutUS.txt"
+const AU_TXT = TXT_AboutUS.split(";");
 
 
 const loadingAboutUs = function(){
@@ -26,13 +27,26 @@ const createAboutSection = function(){
     div.classList.toggle("content-center");
 
     const h1 = document.createElement("h1");
-    h1.textContent = "About Us";
+    h1.textContent = 'About "Them" ?';
 
     div.appendChild(h1);
 
     const p = document.createElement("p");
-    p.textContent = Lorem;
+    p.textContent = AU_TXT[0];
     div.appendChild(p);
+
+    const p2 = document.createElement("p");
+    p2.textContent = AU_TXT[1];
+    div.appendChild(p2);
+
+
+    const link = document.createElement("a");
+    link.textContent = "Le P'tit Breton";
+    link.setAttribute("href", "https://www.leptitbreton-creperie.com");
+    link.setAttribute("rel", "noreferrer noopener");
+    link.setAttribute("target", "_blank");
+    div.appendChild(link)
+    
 
     return div;
 }
