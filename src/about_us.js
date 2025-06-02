1b477971c198d432aa9f37f3568f69bc17ef2e5a
Oreus-F <fourniol.lucas@gmail.com>
2025-06-02 16:54:05 +0200

    Create LoadingAboutUs

---
+++ src/about_us.js
@@ -0,0 +1,35 @@
+const loadingAboutUs = function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("about_us");
+
+
+    const content = document.querySelector("#content");
+    const div = document.createElement("div");
+
+    div.setAttribute("id", "aboutUs");
+
+
+    div.appendChild(createAboutSection());
+
+
+    content.appendChild(div);
+
+    
+};
+
+
+const createAboutSection = function(){
+    const div = document.createElement("div");
+    div.classList.toggle("content-center");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = "About Us";
+
+    div.appendChild(h1);
+
+    return div;
+}
+
+
+
+export {loadingAboutUs};
