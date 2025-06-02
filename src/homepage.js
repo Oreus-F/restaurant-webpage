5493d6a133836f6f38f042a04636738ef86c80ce
Oreus-F <fourniol.lucas@gmail.com>
2025-06-02 15:00:10 +0200

    add .homepage to header for styling text-shadow

--- src/homepage.js
+++ src/homepage.js
@@ -2,12 +2,15 @@ import mainImg from "/asset/img/mainGalette.jpeg"
 
 
 const loading_homepage = function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("homepage");
+    
     const banderole = document.createElement("div");
     banderole.setAttribute("id", "banderole");
 
     const p1 = document.createElement("p");
     const p2 = document.createElement("p");
-    p1.textContent = "Breizh in our hearts";
+    p1.textContent = "Breizh in our hearts:";
     p2.textContent = "Hapiness in your plate";
 
     banderole.appendChild(p1);
