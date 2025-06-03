927cb171b777c48c05fbacbd868350e0fcd7c51f
Oreus-F <fourniol.lucas@gmail.com>
2025-06-03 17:57:51 +0200

    Import false subway station and let instructions for next day

--- src/about_us.js
+++ src/about_us.js
@@ -1,6 +1,8 @@
 import TXT_AboutUS from "../asset/txt/AboutUS.txt"
 const AU_TXT = TXT_AboutUS.split(";");
 
+import metroIcon from "../asset/img/metro.svg";
+
 
 const loadingAboutUs = function(){
     const header = document.querySelector("header");
@@ -58,11 +60,53 @@ const createContact = function(){
     const div = document.createElement("div");
     div.classList.toggle("content-center");
 
+    const h1 = document.createElement('h1');
+    h1.textContent = "Contact";
+    div.appendChild(h1);
 
+    const container = document.createElement("div");
+    const img= document.createElement("img");
+
+
+    container.appendChild(img);
+
+    const name = document.createElement("p");
+    name.textContent = "Odin's crêperie";
+    const adress = document.createElement("p");
+    adress.textContent = "11 rue d'Odin";
+    const hour = document.createElement("p");
+    hour.textContent = "12.00 - 15.00  / 19.00 - 22.30";
+
+    const metro = document.createElement("p");
+    const span = document.createElement("span");
+    const iconImg = document.createElement("img");
+    iconImg.src = metroIcon;
+    iconImg.setAttribute("height", "20px");
+    iconImg.setAttribute("width", "20px");
+    
+    span.appendChild(iconImg);
+    metro.appendChild(span);
+
+    const span2 = document.createElement("span");
+    span2.textContent = "Somewhere Station"
+    metro.appendChild(span2)
+
+    /* 
+    
+    - trouver meilleure icon pour metro et bus 
+    - soit trouver la bonne couleur soit la changer (icon)
+    - placer faux numéro de tel 
+    - créer le grid element pour tout placer
+    - garder les écrans en full screen pour les futurs travaux ! 
+    
+    */
+    
+    
 
 
     return div
 }
+
 
 
 
