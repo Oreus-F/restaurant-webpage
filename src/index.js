485a6942919eb6e3745a124aa3dccc02dde492d6
Oreus-F <fourniol.lucas@gmail.com>
2025-06-04 19:09:03 +0200

    link nav buttons with it's own function call

--- src/index.js
+++ src/index.js
@@ -5,9 +5,24 @@ import { loadingMenu } from "./menu";
 import displayReservation from "./calendar";
 import { loadingAboutUs } from "./about_us";
 
+const loadingFunctions = [loading_homepage, loadingMenu, loadingAboutUs, displayReservation];
 
-resetDOM();
-// loading_homepage();
-// loadingMenu();
-// displayReservation();
-loadingAboutUs()
+const nav = document.querySelector("nav");
+const buttons = nav.querySelectorAll("button");
+
+for (let x = 0; x < buttons.length; x++){
+    if (x === 3){
+        buttons[x].addEventListener("click", () => {
+            loadingFunctions[x]();
+        });
+
+        break
+    };
+
+    buttons[x].addEventListener("click", () => {
+        resetDOM();
+        loadingFunctions[x]();
+    })
+}
+
+loading_homepage();
